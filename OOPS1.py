95e2ea4829c0014842a6c1b08bda38e4a1c5e8f5
SarathNarasimhan <sarathknarasimhan@gmail.com>
2019-08-07 18:14:23 +0530

    Introduction to OOPS. Class, Objects.

---
+++ OOPS1.py
@@ -0,0 +1,13 @@
+class computer():
+    def config(self):
+        print('This is sarath')
+
+
+com1 = computer()
+com2 = computer()
+
+computer.config(com1)
+computer.config(com2)
+
+com1.config() #alternative for line 9
+com2.config() #alternative for line 10
