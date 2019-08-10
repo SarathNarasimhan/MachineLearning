e1c35234706e5d2fe47ddf0f66f4e05754d9f52c
SarathNarasimhan <53570996+SarathNarasimhan@users.noreply.github.com>
2019-08-10 12:14:11 +0530

    Update OOPS1.py

--- OOPS1.py
+++ OOPS1.py
@@ -1,13 +1,9 @@
-class computer():
-    def config(self):
-        print('This is sarath')
+def config(self):
+        print("i5, 16GB, 1TB")
 
 
 com1 = computer()
 com2 = computer()
 
-computer.config(com1)
-computer.config(com2)
-
-com1.config() #alternative for line 9
-com2.config() #alternative for line 10
+com1.config() 
+com2.config() 
